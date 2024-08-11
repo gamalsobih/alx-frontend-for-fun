e4e420fb571b927d8b71c679daa7f00222c607a0
gamal sob <133385330+gamalsobih@users.noreply.github.com>
2024-08-11 05:32:17 +0300

    Update markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -8,7 +8,7 @@ import sys
 import os.path
 import re
 import hashlib
-if __name__!='__main__' :
+if __name__=='__main__' :
     
     if len(sys.argv) !=3 :
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
