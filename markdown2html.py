48fc454b907c52c95452c3e5116fb9bced670aae
gamal sob <133385330+gamalsobih@users.noreply.github.com>
2024-08-11 05:33:40 +0300

    Update markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -12,7 +12,7 @@ if __name__=='__main__' :
     
     if len(sys.argv) !=3 :
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
-        exit(0)
+        sys.exit(1)
 
     filname_input=sys.argv[1]
     filename_outbut=sys.argv[2]
